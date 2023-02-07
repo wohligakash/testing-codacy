67b23c927f8c82c9349d230608b6485afa5955bb
wohligakash <akash.pawar@wohlig.com>
2023-02-07 12:35:20 +0530

    changes

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express")
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello DevOps Team - you all are a great team")
+  res.send("Hello DevOps Team")
 })
 
 app.listen(8050, () => {
